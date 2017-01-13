955ddfa04497618cee07a7f6c4f343167151d4b6
xuxiaoyu <xuxiaoyuspallow@gmail.com>
2017-01-13 11:50:15 +0800

    slidingwindowmaximum

---
+++ algorithms/SlidingWindowMaximum.py
@@ -0,0 +1,29 @@
+"""
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+For example,
+Given nums = [1,3,-1,-3,5,3,6,7], and k = 3.
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Therefore, return the max sliding window as [3,3,5,5,6,7].
+"""
+
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        return [max(nums[i:i + k]) for i in range(len(nums) - k + 1)] if nums else []
+
+"""
+The oneline answer above beats about 8% answerers of the question,which means it is a terrible answer
+"""
