5addad7015abc1a379623958ef1d2303c74d05d0
xuxiaoyu <xuxiaoyuspallow@gmail.com>
2017-02-08 13:02:15 +0800

    Two sum

---
+++ leetcode/TwoSum.py
@@ -0,0 +1,42 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+"""
+
+
+class Solution(object):
+    # def twoSum(self, nums, target):
+    #     """
+    #     O(n^2) my solution
+    #     """
+    #     """
+    #     :type nums: List[int]
+    #     :type target: int
+    #     :rtype: List[int]
+    #     """
+    #     for i,x1 in enumerate(nums):
+    #         for j,x2 in enumerate(nums):
+    #             if x1 + x2 == target and i != j:
+    #                 return [i ,j]
+
+    def twoSum(self,nums,target):
+        """
+        O(n) also my solution
+        """
+        buff_dict = {}
+        for i, n in enumerate(nums):
+            if target - n in buff_dict:
+                return [buff_dict[target - n], i]
+            else:
+                buff_dict[n] = i
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.twoSum([4,2,1],6))
