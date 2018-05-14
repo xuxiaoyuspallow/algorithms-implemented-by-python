2a1c4b77cb1e7a815fb78173b7ab0b157a6c8656
xuxiaoyuspallow <xuxiaoyuspallow@gmail.com>
2018-05-14 18:34:40 +0800

    [Largest Rectangle in Histogram] it is a nice way to learn how to write a height-wildth program, using stack

---
+++ leetcode/array/Largest Rectangle in Histogram.py
@@ -0,0 +1,36 @@
+"""
+84.Largest Rectangle in Histogram
+Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+
+
+Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
+
+
+
+
+The largest rectangle is shown in the shaded area, which has area = 10 unit.
+
+
+
+Example:
+
+Input: [2,1,5,6,2,3]
+Output: 10
+"""
+
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        heights.append(0)
+        stack = [-1]
+        ans = 0
+        for i in range(len(heights)):
+            while heights[i] < heights[stack[-1]]:
+                h = heights[stack.pop()]
+                w = i - stack[-1] - 1
+                ans = max(h*w,ans)
+            stack.append(i)
+        return ans
