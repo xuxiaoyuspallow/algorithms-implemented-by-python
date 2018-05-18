9770b66239b4fca24d35c5f4eccf8eafca04fd61
xuxiaoyuspallow <xuxiaoyuspallow@gmail.com>
2018-05-18 18:34:23 +0800

    word break

---
+++ leetcode/dp/word break.py
@@ -0,0 +1,42 @@
+"""
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note:
+
+The same word in the dictionary may be reused multiple times in the segmentation.
+You may assume the dictionary does not contain duplicate words.
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet", "code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+             Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output: false
+"""
+
+
+class Solution:
+    # @param s, a string
+    # @param dict, a set of string
+    # @return a boolean
+
+    def wordBreak(self, s, dict):
+        if s == '':
+            return True
+        checklist = [False] * (len(s) + 1)
+        checklist[len(s)] = True
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(i, len(s)):
+                if s[i:j + 1] in dict and checklist[j + 1] == True:
+                    checklist[i] = True
+        return checklist[0]
+
+Solution().wordBreak('leetcode',["leet", "code"])
